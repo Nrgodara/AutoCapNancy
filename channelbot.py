4c3de1c5edfd462fe51c99f9f2bbfd8fa3031cea
MAHI® <155721977+Nrgodara@users.noreply.github.com>
2024-12-03 08:35:36 +0530

    Update channelbot.py

--- channelbot.py
+++ channelbot.py
@@ -1,15 +1,23 @@
 import Config
+import ntplib
 import logging
 from pyromod import listen
 from pyrogram import Client, idle
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
-
 
 logging.basicConfig(
     level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+
+try:
+    client = ntplib.NTPClient()
+    response = client.request('pool.ntp.org')
+    time_offset = response.tx_time - time.time()
+    time.sleep(time_offset)  # Adjust local time offset
+except Exception as e:
+    print(f"Time sync failed: {e}")
 
 app = Client(
     ":memory:",
